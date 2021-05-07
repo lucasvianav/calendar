bfe99dfefa8c043253b5d2517eb779cad68067f1
Lucas Viana <oviana@outlook.com>
2021-05-07 09:55:24 -0300

    backend - auth service - signin() - await for async account search

--- back-end/services/auth.js
+++ back-end/services/auth.js
@@ -2,12 +2,14 @@ const Account = require('../models/account')
 
 const authService = {
     signup: async (name, email, googleId) => {
+        // if any information is missing
         if(!name || !email || !googleId){
             return { status: 400, json: { message: 'Unfortunately, your signup failed because of lack of information.' } }
         }
 
         const response = {}
 
+        // tries and creates the accoutn
         try{
             response.json = await Account.create({name, email, googleId})
             response.status = 200
@@ -26,7 +28,7 @@ const authService = {
     },
 
     signin: async (googleId) => {
-        const user = Account.findOne({googleId})
+        const user = await Account.findOne({googleId})
         
         return user ? { status: 200, json: user } : { status: 404, json: { message: 'Login failed because account was not found.' } }
         
