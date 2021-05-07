8beb95bc5a6433b90de99e5ec4a0d49a75303551
Lucas Viana <oviana@outlook.com>
2021-05-07 10:08:29 -0300

    backend - index.js comments

--- back-end/index.js
+++ back-end/index.js
@@ -1,24 +1,31 @@
 const cors = require('cors')
 const express = require('express')
+
+const app = express() // server app
+const routers = require('./routers') // request endpoints
 const mongoose = require('mongoose')
-const app = express()
+const db = require('./config/mongodb') // database
 
-const apiRouter = require('./routers')
-const db = require('./connection')
 
+// Require Passport config
+// require('./config/passport')
+
+// get .env file's data
 require('dotenv').config();
 
+// avoid deprecated congif
 mongoose.set('useFindAndModify', false)
 
+// request body parsers
 app.use(express.json({limit: '30mb'}))
 app.use(express.urlencoded({ extended: true }))
 
-const corsOptions = {
-    origin: 'http://localhost:' + process.env.CLIENT_PORT,
-    optionsSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
-app.use('/api', apiRouter)
+// enable requests from different origins
+app.use(cors({ origin: 'http://localhost:' + process.env.CLIENT_PORT, optionsSuccessStatus: 200, }))
 
+// setup routers
+app.use('/api', routers)
+
+// start server
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => app.listen(process.env.SERVER_PORT, () => console.log(`Example app listening at http://localhost:` + process.env.SERVER_PORT)))
