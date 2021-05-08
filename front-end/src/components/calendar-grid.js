c980197ae105488c81812a642c15041e842d7178
Lucas Viana <oviana@outlook.com>
2021-05-08 16:15:45 -0300

    frontend modularization - calendar grid component

---
+++ front-end/src/components/calendar-grid.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { GridItem } from '@chakra-ui/layout'
+
+class CalendarGrid extends React.Component {
+    render = () => (
+        <>
+        {
+            Array.from({length: 24*7}, (_, i) => (
+                <GridItem 
+                    rowSpan={60} colSpan={1} 
+                    rowStart={parseInt(i/7)*60+2} colStart={i%7+2} 
+                    borderX='1px' borderY='1px' borderColor='gray.300' 
+                    m='0px' padding='1%'
+                />
+            ))
+        }
+        </>
+    )
+}
+
+export default CalendarGrid
