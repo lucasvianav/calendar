44846e1056c4b418d55f3198fb7f8b4c65796a22
Lucas Viana <oviana@outlook.com>
2021-05-08 16:16:01 -0300

    frontend modularization - calendar times component

---
+++ front-end/src/components/calendar-times.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { GridItem, Text } from '@chakra-ui/layout'
+
+class CalendarTimes extends React.Component {
+    render = () => (
+        <>
+            {
+                Array.from({length: 23}, (_, i) => (
+                    <GridItem rowSpan={60} colSpan={1} rowStart={(i+1)*60} colStart={1} m='0' mr='7%'>
+                        <Text textAlign='right' fontSize='6pt' color='grey' w='100%' h='100%' m='0' p='0'>
+                            {i < 12 ? (i+1).toString() + ' AM' : (i-11).toString() + ' PM'}
+                        </Text>
+                    </GridItem>
+                ))
+            }
+        </>
+    )
+}
+
+export default CalendarTimes
