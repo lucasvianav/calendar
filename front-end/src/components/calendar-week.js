1af531552556ab649fb9320dc21731aa970b71c2
Lucas Viana <oviana@outlook.com>
2021-05-08 16:17:27 -0300

    frontend - including components into calendar-week screen

--- front-end/src/components/calendar-week.js
+++ front-end/src/components/calendar-week.js
@@ -1,41 +1,38 @@
-import { Box, Flex, Grid, GridItem, Text, Heading } from '@chakra-ui/layout'
+import { Flex, Grid } from '@chakra-ui/layout'
 import React from 'react'
+import CalendarDates from './calendar-dates'
+import CalendarEvent from './calendar-event'
+import CalendarGrid from './calendar-grid'
+import CalendarTimes from './calendar-times'
+import CalendarTodayHighlight from './calendar-today-highlight'
 
 const minutesInDay = 60*24
-const weekdays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb']
 
 class CalendarWeek extends React.Component {
+    constructor(props){
+        super(props)
+
+        const sunday = new Date()
+        while(sunday.getDay() > 0){ sunday.setDate(sunday.getDate()-1) }
+
+        this.state = { sunday }
+    }
+
     render = () => (
-        <Flex justify='center'>
-            <Grid templateRows={`repeat(${minutesInDay+1}, 1fr)`} templateColumns='repeat(8, 1fr)' p={[0, 2, 0, 2]} w='95%' h='1111px' gap={0}>
-                {
-                    Array.from({length: 23}, (_, i) => (
-                        <GridItem rowSpan={60} colSpan={1} rowStart={i*60+3} colStart={1} w='15%' bg='pink.300' mr='0px'>
-                            <Text textAlign='right' fontSize='6pt' color='grey' w='100%' h='100%'>
-                                {i < 12 ? (i+1).toString() + ' AM' : (i-11).toString() + ' PM'}
-                            </Text>
-                        </GridItem>
-                    ))
-                }
+        <Flex justify='center' mr='9%' my='2%'>
+            <Grid templateRows={`repeat(${minutesInDay+1}, 1fr)`} templateColumns='repeat(8, 1fr)' p={[0, 2, 0, 2]} w='100%' h='1111px' gap={0}>
+                {/* sets up calendar grid (borders) */}
+                <CalendarGrid/>
 
-                {
-                    weekdays.map((day, i) => (
-                        <GridItem rowSpan={1} colSpan={1} rowStart={1} colStart={i%7+2} m='0px' mb='7px' bg="">
-                            <Heading as='h6' size='xs' color='gray.600' textAlign='center'>{day}</Heading>
-                            <Heading as='h3' size='lg' color='gray.700' textAlign='center'>{i+1}</Heading>
-                        </GridItem>
-                    ))
-                }
+                <CalendarTodayHighlight/>
 
-                {
-                    Array.from({length: 24*7}, (_, i) => (
-                        <GridItem rowSpan={60} colSpan={1} rowStart={parseInt(i/7)*60+2} colStart={i%7+2} m='0px' borderX='1px' borderY='1px' borderColor='gray.300' padding='5%' textAlign='center'>
-                            <Text backgroundColor="orange.500" w='100%' h='100%'>
-                                oi
-                            </Text>
-                        </GridItem>
-                    ))
-                }
+                {/* sets up calendar times */}
+                <CalendarTimes/>
+
+                {/* sets up calendar dates */}
+                <CalendarDates sunday={this.state.sunday}/>
+        
+                <CalendarEvent startDate={'2021-05-04 10:19'} endDate={'2021-05-04 11:36'}/>
             </Grid>
         </Flex>
     )
